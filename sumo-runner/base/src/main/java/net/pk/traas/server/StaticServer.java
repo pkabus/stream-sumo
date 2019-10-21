2d87969068c440575a56426199b57f1e98025165
Peter Kabus <peter.kabus@campus.tu-berlin.de>
2019-10-21 15:29:53 +0200

    StaticServer extends CoachedServer
    to set tls program to "0" for each tls

--- sumo-runner/base/src/main/java/net/pk/traas/server/StaticServer.java
+++ sumo-runner/base/src/main/java/net/pk/traas/server/StaticServer.java
@@ -1,12 +1,29 @@
 package net.pk.traas.server;
 
+import java.util.List;
+
 import de.tudresden.sumo.cmd.Simulation;
+import de.tudresden.sumo.cmd.Trafficlight;
 
 /**
  * @author peter
  *
  */
-public class StaticServer extends TraasServer {
+public class StaticServer extends CoachedServer {
+
+	@Override
+	protected void beforeSimulation() {
+		super.beforeSimulation();
+		try {
+			@SuppressWarnings("unchecked")
+			List<String> tlsIds = (List<String>) getConnection().do_job_get(Trafficlight.getIDList());
+			for (String tlsID : tlsIds) {
+				getConnection().do_job_set(Trafficlight.setProgram(tlsID, "0"));
+			}
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+	}
 
 	@Override
 	protected void doSimulation() throws Exception {
