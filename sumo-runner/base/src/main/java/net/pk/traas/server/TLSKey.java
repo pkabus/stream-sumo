73c41c5ecf900379838a0467fb1b940173c95504
Peter Kabus <peter.kabus@campus.tu-berlin.de>
2019-10-22 11:29:40 +0200

    implemented equals method for TLSKey
    and now using a static set instead of a list to store TLSKey objects

--- sumo-runner/base/src/main/java/net/pk/traas/server/TLSKey.java
+++ sumo-runner/base/src/main/java/net/pk/traas/server/TLSKey.java
@@ -1,31 +1,53 @@
 package net.pk.traas.server;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashSet;
+import java.util.Set;
 
 import org.apache.commons.lang3.StringUtils;
 
 /**
+ * This class represents a key or id of a certain TLS in Sumo. Also a static set
+ * exists, that is used to store the keys when a certain scenario is running.
+ * 
  * @author peter
  *
  */
 public class TLSKey {
 
-	private static List<TLSKey> list = new ArrayList<>();
+	private static Set<TLSKey> set = new HashSet<>();
 
 	private String id;
 
+	/**
+	 * Add the given object to the static list of registered {@link TLSKey} objects.
+	 * 
+	 * @param obj
+	 */
 	protected static void add(final TLSKey obj) {
-		if (list.stream().noneMatch(k -> k.getId().equals(obj.getId()))) {
-			list.add(obj);
-		}
+		set.add(obj);
 	}
 
+	/**
+	 * Checks if there is a {@link TLSKey} object that has an id which is equal to
+	 * the end of the given edgeId. Conventions:
+	 * <ul>
+	 * <li>tls.id == junciton.id</li>
+	 * <li>edge.id = edge.from + separator + edge.to</li>
+	 * </ul>
+	 * 
+	 * @param edgeId to check
+	 * @return object related to given edgeId
+	 */
 	public static TLSKey findByEdgeId(final String edgeId) {
-		return list.stream().filter(tls -> StringUtils.endsWith(edgeId, tls.id)).findFirst()
+		return set.stream().filter(tls -> StringUtils.endsWith(edgeId, tls.id)).findFirst()
 				.orElseThrow(() -> new RuntimeException("No tls found that fits " + edgeId));
 	}
 
+	/**
+	 * Constructor.
+	 * 
+	 * @param id of tls
+	 */
 	public TLSKey(final String id) {
 		this.id = id;
 	}
@@ -37,6 +59,11 @@ public class TLSKey {
 	 */
 	public String getId() {
 		return id;
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		return (o instanceof TLSKey) ? ((TLSKey) o).id.equals(this.id) : false;
 	}
 
 	@Override
