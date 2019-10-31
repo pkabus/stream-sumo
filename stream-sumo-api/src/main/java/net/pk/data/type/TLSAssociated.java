ea975c7c121bc35d30105d0282e752be85bbe96d
Peter Kabus <peter.kabus@campus.tu-berlin.de>
2019-10-31 11:14:21 +0100

    Added javadoc

--- stream-sumo-api/src/main/java/net/pk/data/type/TLSAssociated.java
+++ stream-sumo-api/src/main/java/net/pk/data/type/TLSAssociated.java
@@ -1,6 +1,18 @@
 package net.pk.data.type;
 
+/**
+ * Implementations using this interface label themselves as associated to a TLS
+ * in any sense. They need to have a connection to a TLS.
+ * 
+ * @author peter
+ *
+ */
 public interface TLSAssociated {
 
+	/**
+	 * Associated TLS id of this object.
+	 * 
+	 * @return tls id
+	 */
 	String getTLS();
 }
